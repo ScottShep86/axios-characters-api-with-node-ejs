236360625ae34389aabb152548c083ad5be5f874
Scott Shepherd <scottshep86@gmail.com>
2023-05-15 08:56:26 +0200

    done until edit/delete character

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -11,6 +11,10 @@ router.get("/characters", (req, res, next) => {
     .catch(err => console.error(err))
 });
 
+router.get("/characters/create", (req, res, next) => {
+    res.render("characters/create-character")
+})
+
 
 router.get("/characters/:id", (req, res, next) => {
     axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
@@ -21,7 +25,34 @@ router.get("/characters/:id", (req, res, next) => {
     .catch(err => console.error(err))
 });
 
+router.post("/characters/create", async (req, res, next) => {
+    try {
+        const {name, occupation, weapon, debt } = req.body
+    axios.post("https://ih-crud-api.herokuapp.com/characters", {
+        name, occupation, weapon, debt
+    })
+    .then(responseFromAPI => {
+        console.log(responseFromAPI)
+        res.redirect("/characters")
+    })
+} catch (error) {
+    console.log(error)
+}
+})
+
+
+
+
+
+/* router.post("/characters/create"), (req, res, next) => {
+    axios.post(`https://ih-crud-api.herokuapp.com/characters`)
+    .then(responseFromAPI => {
+        res.redirect("characters")
+    })
+} */
+
 module.exports = router;
+
 
 
 // https://ih-crud-api.herokuapp.com/characters
